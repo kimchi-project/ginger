2e382c532ee359ed9294f550f307c5e6fb1760ae
Zhou Zheng Sheng <zhshzhou@linux.vnet.ibm.com>
2014-02-25 13:45:53 +0800

    RootModel: replace RootModel with kimchi.basemodel.BaseModel
    The code of RootModel is the same as BaseModel. Now that BaseModel is
    merged into kimchi-base, we can drop RootModel and use BaseModel
    instead.
    
    Change-Id: I939440b639c9f20ae976f08641c9d96aa555a7c6
    Signed-off-by: Zhou Zheng Sheng <zhshzhou@linux.vnet.ibm.com>
    Reviewed-on: http://9.181.129.215/58
    Reviewed-by: Mark Wu <wudxw@linux.vnet.ibm.com>

--- models/model.py
+++ models/model.py
@@ -22,42 +22,13 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 
 from interfaces import InterfacesModel, InterfaceModel
+from kimchi.basemodel import BaseModel
 from network import NetworkModel
 from powermanagement import PowerProfilesModel, PowerProfileModel
 from users import UsersModel, UserModel
 
 
-# RootModel is taken from Zhengsheng's patch for kimchi upstream.
-# We could remove it from here when it's merged there.
-
-
-class RootModel(object):
-    '''
-    This model squashes all sub-model's public callable methods to itself.
-
-    Model methods are not limited to get_list, create, lookup, update, delete.
-    Controller can call generate_action_handler to generate new actions, which
-    call the related model methods. So all public callable methods of a
-    sub-model should be mapped to this model.
-    '''
-    def __init__(self, model_instances):
-        for model_instance in model_instances:
-            cls_name = model_instance.__class__.__name__
-            if cls_name.endswith('Model'):
-                method_prefix = cls_name[:-len('Model')].lower()
-            else:
-                method_prefix = cls_name.lower()
-
-            callables = [m for m in dir(model_instance)
-                         if not m.startswith('_') and
-                         callable(getattr(model_instance, m))]
-
-            for member_name in callables:
-                m = getattr(model_instance, member_name, None)
-                setattr(self, '%s_%s' % (method_prefix, member_name), m)
-
-
-class GingerModel(RootModel):
+class GingerModel(BaseModel):
 
     def __init__(self):
         sub_models = []
