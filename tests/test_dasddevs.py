a768c05c767807f67e0438af8fdc9b27737bc960
Pawan Kumar <pawan@linux.vnet.ibm.com>
2015-11-24 12:04:56 +0530

    Support DASD (ECKD) devices : Test Cases
    Test cases to support DASD (ECKD) devices
    adding capability to list, format of type DASD devices
    list, create, delete of type DASD partitions
    
    Signed-off-by: Pawan Kumar <pawan@linux.vnet.ibm.com>

---
+++ tests/test_dasddevs.py
@@ -0,0 +1,75 @@
+#
+# Project Ginger
+#
+# Copyright IBM, Corp. 2015
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+
+import unittest
+
+from wok.plugins.ginger.models.dasd_utils import _parse_lsdasd_output
+
+
+class DASDdevsTests(unittest.TestCase):
+
+    def test_lsdasd_parser(self):
+        lsdasd_out = """0.0.5149/dasdgb/94:732
+  status:       active
+  type:         ECKD
+  blksz:        4096
+  size:         7043MB
+  blocks:       1803060
+  use_diag:     0
+  readonly:     0
+  eer_enabled:  0
+  erplog:       0
+  uid:          IBM.750000000DX111.0002.49
+
+0.0.5150/dasdc/94:8
+  status:       active
+  type:         ECKD
+  blksz:        4096
+  size:         23034MB
+  blocks:       5896800
+  use_diag:     0
+  readonly:     0
+  eer_enabled:  0
+  erplog:       0
+  uid:          IBM.750000000DX111.0002.50"""
+        parse_out = _parse_lsdasd_output(lsdasd_out)
+        if parse_out[0]['status'] != 'active':
+            self.fail("Parsing of lsdasd failed : status")
+        if parse_out[0]['blocks'] != '1803060':
+            self.fail("Parsing of lsdasd failed : blocks")
+        if parse_out[0]['name'] != 'dasdgb':
+            self.fail("Parsing of lsdasd failed : name")
+        if parse_out[0]['uid'] != 'IBM.750000000DX111.0002.49':
+            self.fail("Parsing of lsdasd failed : uid")
+        if parse_out[0]['eer_enabled'] != '0':
+            self.fail("Parsing of lsdasd failed : eer_enabled")
+        if parse_out[0]['erplog'] != '0':
+            self.fail("Parsing of lsdasd failed : erplog")
+        if parse_out[0]['use_diag'] != '0':
+            self.fail("Parsing of lsdasd failed : use_diag")
+        if parse_out[0]['readonly'] != '0':
+            self.fail("Parsing of lsdasd failed : readonly")
+        if parse_out[0]['device'] != '94:732':
+            self.fail("Parsing of lsdasd failed : device")
+        if parse_out[0]['blksz'] != '4096':
+            self.fail("Parsing of lsdasd failed : blksz")
+        if parse_out[0]['bus-id'] != '0.0.5149':
+            self.fail("Parsing of lsdasd failed : bus-id")
+        if parse_out[0]['size'] != '7043MB':
+            self.fail("Parsing of lsdasd failed : size")
---
+++ tests/test_dasdpartitions.py
@@ -0,0 +1,70 @@
+#
+# Project Ginger
+#
+# Copyright IBM, Corp. 2015
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+
+import mock
+import re
+import unittest
+
+import wok.plugins.ginger.models.dasdpartitions as partitions
+from wok.plugins.ginger.models.dasd_utils import _parse_lsblk_output
+
+
+class DASDPartitionsTests(unittest.TestCase):
+
+    @mock.patch('wok.plugins.ginger.models.dasd_utils._create_dasd_part',
+                autospec=True)
+    def test_create_part(self, mock_create_part):
+        parts = partitions.DASDPartitionsModel()
+        dev = 'dasdb'
+        size = '10'
+        params = {'dev_name': dev, 'size': size}
+        parts.create(params)
+        mock_create_part.return_value = 'dasdb'
+        mock_create_part.assert_called_with(dev, size)
+
+    @mock.patch('wok.plugins.ginger.models.dasd_utils._delete_dasd_part',
+                autospec=True)
+    def test_delete_part(self, mock_delete_part):
+        part = partitions.DASDPartitionModel()
+        part_name = 'dasdb2'
+        name_split = re.split('(\D+)', part_name, flags=re.IGNORECASE)
+        dev_name = name_split[1]
+        part_num = name_split[2]
+        part.delete(part_name)
+        mock_delete_part.assert_called_with(dev_name, part_num)
+
+    def test_lsblk_parser(self):
+        lsblk_out = """NAME="dasda" TYPE="disk" FSTYPE="" MOUNTPOINT="" MAJ:MIN="94:0"
+NAME="dasda1" TYPE="part" FSTYPE="ext3" MOUNTPOINT="/" MAJ:MIN="94:1"
+NAME="dasdb" TYPE="disk" FSTYPE="" MOUNTPOINT="" MAJ:MIN="94:4"
+NAME="dasdb1" TYPE="part" FSTYPE="" MOUNTPOINT="" MAJ:MIN="94:5"
+NAME="dasdb2" TYPE="part" FSTYPE="" MOUNTPOINT="" MAJ:MIN="94:6"
+NAME="dasdc" TYPE="disk" FSTYPE="" MOUNTPOINT="" MAJ:MIN="94:8"
+
+"""
+        keys = ["NAME", "TYPE", "FSTYPE", "MOUNTPOINT", "MAJ:MIN"]
+        parse_out = _parse_lsblk_output(lsblk_out, keys)
+        if parse_out[0]['name'] != 'dasda':
+            self.fail("Parsing of lsblk failed : name")
+        if parse_out[0]['type'] != 'disk':
+            self.fail("Parsing of lsblk failed : type")
+        if parse_out[0]['mountpoint'] != '':
+            self.fail("Parsing of lsblk failed : mountpoint")
+        if parse_out[0]['fstype'] != '':
+            self.fail("Parsing of lsblk failed : fstype")
