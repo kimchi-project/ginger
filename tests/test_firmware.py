ff2238d8599faf56d93139084be0977f96f6d213
Daniel Henrique Barboza <dhbarboza82@gmail.com>
2016-01-04 15:02:14 -0200

    Adding unit tests for Firmware model
    Added tests for lookup, update, commit and reject calls.
    
    Signed-off-by: Daniel Henrique Barboza <dhbarboza82@gmail.com>

---
+++ tests/test_firmware.py
@@ -0,0 +1,111 @@
+#
+# Project Ginger
+#
+# Copyright IBM, Corp. 2016
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+
+import mock
+import tempfile
+import unittest
+
+from wok import config
+from wok.exception import OperationFailed
+from wok.objectstore import ObjectStore
+
+from wok.plugins.ginger.models.firmware import FirmwareModel
+
+
+class FirmwareTests(unittest.TestCase):
+
+    def setUp(self):
+        objstore_loc = config.get_object_store() + '_ginger'
+        self._objstore = ObjectStore(objstore_loc)
+
+    @mock.patch('wok.plugins.ginger.models.firmware.run_command')
+    def test_model_lookup(self, mock_run_command):
+        return_output = "0 1 2 3 4 5 6 7 8 9 10 11 12 13"
+        mock_run_command.return_value = [return_output, "", 0]
+        firmware_lookup = FirmwareModel(objstore=self._objstore).lookup()
+        mock_run_command.assert_called_once_with('lsmcode')
+        self.assertEquals(
+            firmware_lookup,
+            {'level': "5 6 7 8 9 10 11 12 13"}
+        )
+
+    @mock.patch('wok.plugins.ginger.models.firmware.run_command')
+    def test_model_lookup_with_product_in_output(self, mock_run_command):
+        return_output = "0 1 2 3 4 Product 6 7 8 9 10 11 12 13"
+        mock_run_command.return_value = [return_output, "", 0]
+        firmware_lookup = FirmwareModel(objstore=self._objstore).lookup()
+        mock_run_command.assert_called_once_with('lsmcode')
+        self.assertEquals(firmware_lookup, {'level': '13'})
+
+    @mock.patch('wok.plugins.ginger.models.firmware.detect_live_vm')
+    def test_model_update_fails_with_running_vm(self, mock_detect_vm):
+        mock_detect_vm.return_value = True
+        with self.assertRaises(OperationFailed):
+            FirmwareModel(objstore=self._objstore).update(None, None)
+
+    @mock.patch('wok.plugins.ginger.models.firmware.detect_live_vm')
+    @mock.patch('wok.plugins.ginger.models.firmware.run_command')
+    def test_model_update(self, mock_run_command, mock_detect_vm):
+        mock_detect_vm.return_value = False
+        mock_run_command.return_value = ["", "", 0]
+
+        temp = tempfile.NamedTemporaryFile()
+        params = {'path': temp.name}
+        command = ['update_flash', '-f', temp.name]
+
+        FirmwareModel(objstore=self._objstore).update(None, params)
+        mock_run_command.assert_called_once_with(
+            command,
+            tee='/tmp/fw_tee_log.txt'
+        )
+
+    @mock.patch('wok.plugins.ginger.models.firmware.detect_live_vm')
+    @mock.patch('wok.plugins.ginger.models.firmware.run_command')
+    def test_model_update_overwrite_perm_false(
+        self,
+        mock_run_command,
+        mock_detect_vm
+    ):
+
+        mock_detect_vm.return_value = False
+        mock_run_command.return_value = ["", "", 0]
+
+        temp = tempfile.NamedTemporaryFile()
+        params = {'path': temp.name, 'overwrite-perm-ok': False}
+        command = ['update_flash', '-n', '-f', temp.name]
+
+        FirmwareModel(objstore=self._objstore).update(None, params)
+        mock_run_command.assert_called_once_with(
+            command,
+            tee='/tmp/fw_tee_log.txt'
+        )
+
+    @mock.patch('wok.plugins.ginger.models.firmware.run_command')
+    def test_model_commit(self, mock_run_command):
+        mock_run_command.return_value = ["", "", 0]
+        command = ['update_flash', '-c']
+        FirmwareModel(objstore=self._objstore).commit(None)
+        mock_run_command.assert_called_once_with(command)
+
+    @mock.patch('wok.plugins.ginger.models.firmware.run_command')
+    def test_model_reject(self, mock_run_command):
+        mock_run_command.return_value = ["", "", 0]
+        command = ['update_flash', '-r']
+        FirmwareModel(objstore=self._objstore).reject(None)
+        mock_run_command.assert_called_once_with(command)
