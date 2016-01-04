d9dd4a8e7fc586db8c7c4a9c04cd97eef85ad6a2
Daniel Henrique Barboza <dhbarboza82@gmail.com>
2016-01-04 16:02:23 -0200

    Unit tests for the PowerManagement model
    In these tests it was necessary to override the __init__ model
    of both PowerProfilesModel and PowerProfileModel due to the
    fact that their initialization is OS dependant (won't work
    unless you're running a RPM distro with tuned-adm installed and
    running).
    
    Signed-off-by: Daniel Henrique Barboza <dhbarboza82@gmail.com>

---
+++ tests/test_powermanagement.py
@@ -0,0 +1,66 @@
+#
+# Project Ginger
+#
+# Copyright IBM, Corp. 2016
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+
+import mock
+import unittest
+
+from wok.plugins.ginger.models.powermanagement import PowerProfilesModel
+from wok.plugins.ginger.models.powermanagement import PowerProfileModel
+
+
+def mock_powerprofiles_init(self):
+    self.error = None
+
+
+def mock_powerprofile_init(self):
+    self.active_powerprofile = 'default'
+
+
+class PowerManagementTests(unittest.TestCase):
+
+    def setUp(self):
+        PowerProfilesModel.__init__ = mock_powerprofiles_init
+        self.power_profiles_model = PowerProfilesModel()
+
+        PowerProfileModel.__init__ = mock_powerprofile_init
+        self.power_profile_model = PowerProfileModel()
+
+    @mock.patch('wok.plugins.ginger.models.powermanagement.run_command')
+    def test_powermanagement_get_list(self, mock_run_command):
+        mock_run_command.return_value = ["", "", 0]
+        self.power_profiles_model.get_list()
+        mock_run_command.assert_called_once_with(['tuned-adm', 'list'])
+
+    def test_powermanagement_lookup(self):
+        lookup = self.power_profile_model.lookup('test_profile')
+        self.assertEqual(lookup, {"name": 'test_profile', "active": False})
+
+    @mock.patch('wok.plugins.ginger.models.powermanagement.run_command')
+    def test_powermanagement_update(self, mock_run_command):
+        mock_run_command.return_value = ["", "", 0]
+        self.power_profile_model.update(
+            'test_active_profile',
+            {'active': True}
+        )
+        tuned_cmd = ["tuned-adm", "profile", "test_active_profile"]
+        mock_run_command.assert_called_once_with(tuned_cmd)
+
+        self.power_profile_model.update('default', {'active': True})
+        tuned_cmd = ["tuned-adm", "profile", "default"]
+        mock_run_command.assert_called_with(tuned_cmd)
