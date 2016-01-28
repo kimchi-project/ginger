cbeb7e9e7b009a166845b3cc944567c070efd571
Daniel Henrique Barboza <dhbarboza82@gmail.com>
2016-01-28 11:00:04 -0200

    sysmodules backend: adding 'mlx4_core' alias
    This module can be loaded by calling either 'mlx4-core' or
    'mlx4_core'. The UI is using the latter, the backend was
    using the former.
    
    This patch allows the backend to accept both forms.
    
    Signed-off-by: Daniel Henrique Barboza <dhbarboza82@gmail.com>

--- model/sysmodules.py
+++ model/sysmodules.py
@@ -197,7 +197,10 @@ class SysModuleModel(object):
             'desc': 'SR-IOV: Single Root I/O Virtualization',
             'parms': ['num_vfs', 'probe_vf']
         }
-        features_mod = {'mlx4-core': {'SR-IOV': mlx4_core_SRIOV}}
+        features_mod = {
+            'mlx4-core': {'SR-IOV': mlx4_core_SRIOV},
+            'mlx4_core': {'SR-IOV': mlx4_core_SRIOV}
+        }
         return features_mod
 
     def __init__(self):
