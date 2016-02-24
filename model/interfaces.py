5ce7c927fe76317c52ad089c9b7a23c314d65320
Aline Manera <alinefm@linux.vnet.ibm.com>
2016-02-24 14:52:44 -0300

    Fix PEP8 issues according to 1.5.7 version
    Signed-off-by: Aline Manera <alinefm@linux.vnet.ibm.com>

--- model/interfaces.py
+++ model/interfaces.py
@@ -214,7 +214,7 @@ class InterfaceModel(object):
                              "activating an interface may not as expected.")
             else:
                 wok_log.info(
-                        'Successfully brought up the interface ' + ifacename)
+                    'Successfully brought up the interface ' + ifacename)
         wok_log.info('Activating an interface ' + ifacename)
         cmd_ifup = ['ifup', '%s' % ifacename]
         out, error, returncode = run_command(cmd_ifup)
@@ -263,7 +263,7 @@ class InterfaceModel(object):
                              "activating an interface may not as expected.")
             else:
                 wok_log.info(
-                        'Successfully brought down the interface ' + ifacename)
+                    'Successfully brought down the interface ' + ifacename)
         vlan_or_bond = [cfginterfaces.IFACE_BOND, cfginterfaces.IFACE_VLAN]
         if iface_type in vlan_or_bond:
             if ifacename in ethtool.get_devices():
