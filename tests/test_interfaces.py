9399aa4f7700fecbd5fcbb4b0c107c3c9c27a27c
Daniel Henrique Barboza <dhbarboza82@gmail.com>
2016-03-11 14:27:26 -0300

    Adding 'module' into interfaces API: test changes
    This patch adds three more tests in tests/test_interfaces.py
    to validate the API change made.
    
    Signed-off-by: Daniel Henrique Barboza <dhbarboza82@gmail.com>

--- tests/test_interfaces.py
+++ tests/test_interfaces.py
@@ -20,6 +20,8 @@
 import mock
 import unittest
 
+import wok.plugins.ginger.model.netinfo as netinfo
+
 from wok.exception import OperationFailed
 from wok.plugins.ginger.model.interfaces import InterfaceModel
 
@@ -75,3 +77,66 @@ class InterfacesTests(unittest.TestCase):
         self.assertRaises(OperationFailed, InterfaceModel().deactivate,
                           interface)
         mock_run_command.assert_called_once_with(cmd)
+
+    @mock.patch('wok.plugins.ginger.model.netinfo.get_loaded_modules_list')
+    @mock.patch('os.readlink')
+    def test_netinfo_interface_module_lookup_success(
+        self, mock_readlink, mock_loaded_mod_list
+    ):
+        mock_loaded_mod_list.return_value = [
+            'mod1', 'mod2', 'dummy_net_module', 'mod3', 'mod4'
+        ]
+        mock_readlink.return_value = '../../../../module/dummy_net_module'
+
+        module = netinfo.get_interface_kernel_module('dummy_iface')
+        mock_loaded_mod_list.assert_called_once_with()
+        mock_readlink.assert_called_once_with(
+           '/sys/class/net/dummy_iface/device/driver/module'
+        )
+
+        self.assertEqual(module, 'dummy_net_module')
+
+    @mock.patch('wok.plugins.ginger.model.netinfo.get_loaded_modules_list')
+    @mock.patch('os.readlink')
+    def test_netinfo_module_lookup_not_loaded_unknown(
+        self, mock_readlink, mock_loaded_mod_list
+    ):
+        mock_loaded_mod_list.return_value = ['mod1', 'mod2', 'mod3', 'mod4']
+        mock_readlink.return_value = '../../../../module/dummy_net_module'
+
+        module = netinfo.get_interface_kernel_module('dummy_iface')
+        self.assertEqual(module, 'unknown')
+        mock_readlink.assert_called_once_with(
+           '/sys/class/net/dummy_iface/device/driver/module'
+        )
+        mock_loaded_mod_list.assert_called_once_with()
+
+    @mock.patch('wok.plugins.ginger.model.netinfo.get_interface_kernel_module')
+    @mock.patch('ethtool.get_devices')
+    @mock.patch('ethtool.get_ipaddr')
+    @mock.patch('ethtool.get_netmask')
+    @mock.patch('wok.plugins.ginger.model.netinfo.macaddr')
+    def test_netinfo_get_interface_info(self, mock_macaddr, mock_netmask,
+                                        mock_ipaddr, mock_getdevs,
+                                        mock_get_module):
+        mock_get_module.return_value = 'dummy_net_module'
+        mock_getdevs.return_value = ['dev1', 'dummy_iface', 'dev2']
+        mock_ipaddr.return_value = '99.99.99.99'
+        mock_netmask.return_value = '255.255.255.0'
+        mock_macaddr.return_value = 'aa:bb:cc:dd:ee:ff'
+
+        iface_info = netinfo.get_interface_info('dummy_iface')
+
+        mock_macaddr.assert_called_once_with('dummy_iface')
+        mock_netmask.assert_called_once_with('dummy_iface')
+        mock_ipaddr.assert_called_once_with('dummy_iface')
+        mock_getdevs.assert_called_once_with()
+        mock_get_module.assert_called_once_with('dummy_iface')
+
+        self.assertEqual(iface_info.get('device'), 'dummy_iface')
+        self.assertEqual(iface_info.get('type'), 'unknown')
+        self.assertEqual(iface_info.get('status'), 'down')
+        self.assertEqual(iface_info.get('ipaddr'), '99.99.99.99')
+        self.assertEqual(iface_info.get('netmask'), '255.255.255.0')
+        self.assertEqual(iface_info.get('macaddr'), 'aa:bb:cc:dd:ee:ff')
+        self.assertEqual(iface_info.get('module'), 'dummy_net_module')
