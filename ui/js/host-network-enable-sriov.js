e8aaa6f083492509fc942cdd5e482d06e7582b68
Daniel Henrique Barboza <dhbarboza82@gmail.com>
2016-03-28 18:31:54 -0300

    ui/js/host-network-enable-sriov.js: code style fixes
    - added copyright header
    - removed the tabs and fixed identation
    
    Signed-off-by: Daniel Henrique Barboza <dhbarboza82@gmail.com>

--- ui/js/host-network-enable-sriov.js
+++ ui/js/host-network-enable-sriov.js
@@ -1,32 +1,50 @@
+/*
+ * Copyright IBM Corp, 2016
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 ginger.initEnableSriov = function() {
-	$("#modal-interface-name").text(ginger.selectedInterface['device']);
-	ginger.enableJustNumbers();
+    $("#modal-interface-name").text(ginger.selectedInterface['device']);
+    ginger.enableJustNumbers();
 
     $(".save-button").on('click', function() {
-    	var data = {
-    		"name": "SR-IOV",
-    		"args": {
-    			"num_vfs": $("#number-virtual-functions").val()
-    		}
-    	};
-    	ginger.enableNetworkSRIOV(data, function(result){
-    		ginger.initNetworkConfigGridData();
-    	}, function(error){
-    		wok.message.error(error.responseJSON.reason, '#message-nw-container-area', true);
-    	});
+        var data = {
+            "name": "SR-IOV",
+            "args": {
+                "num_vfs": $("#number-virtual-functions").val()
+            }
+        };
+        ginger.enableNetworkSRIOV(data, function(result){
+            ginger.initNetworkConfigGridData();
+        }, function(error){
+            wok.message.error(error.responseJSON.reason, '#message-nw-container-area', true);
+        });
     });
 };
 
 ginger.enableJustNumbers = function() {
-	$("#number-virtual-functions").keydown(function (e) {
-        // Allow: backspace, delete, tab, escape, enter and .
-        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
-             // Allow: Ctrl+A, Command+A
-            (e.keyCode == 65 && ( e.ctrlKey === true || e.metaKey === true ) ) ||
-             // Allow: home, end, left, right, down, up
-            (e.keyCode >= 35 && e.keyCode <= 40)) {
-                 // let it happen, don't do anything
-                 return;
+    $("#number-virtual-functions").keydown(function (e) {
+    // Allow: backspace, delete, tab, escape, enter and .
+    if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+        // Allow: Ctrl+A, Command+A
+        (e.keyCode == 65 && ( e.ctrlKey === true || e.metaKey === true ) ) ||
+        // Allow: home, end, left, right, down, up
+        (e.keyCode >= 35 && e.keyCode <= 40)) {
+            // let it happen, don't do anything
+            return;
         }
         // Ensure that it is a number and stop the keypress
         if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
@@ -34,4 +52,3 @@ ginger.enableJustNumbers = function() {
         }
     });
 }
-
