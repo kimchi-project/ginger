1bb2ec574a63b5170aa070c282d835f83b269c89
Atreyee Mukhopadhyay <atreyee@linux.vnet.ibm.com>
2016-11-02 11:26:12 +0530

    Issue # 454: File system filter not working.
    data type of the column was not matching with the converter
    mentioned for the column.

--- ui/js/host-storage.js
+++ ui/js/host-storage.js
@@ -62,7 +62,7 @@ ginger.loadFileSystemDetails = function() {
     "title": i18n['GINTITLE0004M']
   }, {
     "column-id": "use%",
-    "type": 'string',
+    "type": 'numeric',
     "title": i18n['GINTITLE0005M'],
     "width": "35%",
     "formatter": "percentage-used"
